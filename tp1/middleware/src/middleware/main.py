b8baa01b259f16052ce9880d02bda7d3b63eaace
luciakasman <lucia.kasman@gmail.com>
2023-10-08 16:43:01 -0300

    merge changes

--- tp1/middleware/src/middleware/main.py
+++ tp1/middleware/src/middleware/main.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD:middleware/src/main.py
-from middleware import Middleware
-from publisher_consumer import PublisherConsumer
-# from publisher_suscriber import PublisherSuscriber
-=======
 from .middleware import Middleware
 from .publisher_consumer import PublisherConsumer
 from .publisher_suscriber import PublisherSuscriber
->>>>>>> f2229d0cba38a77b28697bbb434b8f6824443fa0:tp1/middleware/src/middleware/main.py
 
 # middleware1 = Middleware(PublisherSuscriber())
 # middleware1.send_message("ffdsfds")
